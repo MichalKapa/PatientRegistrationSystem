6cdbdb943cc9526e9c804eafe44055f787666b46
MichalKapa <michal.kapala05@gmail.com>
2023-06-08 00:38:06 +0200

    Added other roles' headers.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,18 +2,63 @@ import "../styles/Header.scss";
 import HeaderButton from "./HeaderButton";
 
 const Header = () => {
-    const role: string = 'g'
+    const role: string = 'p'
     switch(role) {
         case 'p': {
             return (
             <nav id='header'>
-                
+                <ul>
+                    <li>
+                        <HeaderButton text={"STRONA GŁÓWNA"} primaryColor={"black"} secondaryColor={"white"} link={"/"} />
+                    </li>
+                    <li>
+                        <HeaderButton text={"KONTAKT"} primaryColor={"black"} secondaryColor={"white"} link={"/contact"} />
+                    </li>
+                    <li>
+                        <HeaderButton text={"MOJE DANE"} primaryColor={"black"} secondaryColor={"white"} link={"/my/data"} />
+                    </li>
+                    <li>
+                        <HeaderButton text={"MOJE WIZYTY"} primaryColor={"black"} secondaryColor={"white"} link={"/my/reservations"} />
+                    </li>
+                    <li>
+                        <a href={"/"} className='buttons logout_button'>WYLOGUJ</a>
+                    </li>
+                </ul>
             </nav>
             )}
         case 'd': {
             return (
             <nav id='header'>
-               
+               <ul>
+                    <li>
+                        <HeaderButton text={"STRONA GŁÓWNA"} primaryColor={"black"} secondaryColor={"white"} link={"/"} />
+                    </li>
+                    <li>
+                        <HeaderButton text={"KONTAKT"} primaryColor={"black"} secondaryColor={"white"} link={"/contact"} />
+                    </li>
+                    <li>
+                        <HeaderButton text={"KALENDARZ"} primaryColor={"black"} secondaryColor={"white"} link={"/doctor/calendar"} />
+                    </li>
+                    <li>
+                        <a href={"/"} className='buttons logout_button'>WYLOGUJ</a>
+                    </li>
+               </ul>
+            </nav>
+            )}
+        case 'a': {
+            return (
+            <nav id='header'>
+                <ul>
+                    <li>
+                        <HeaderButton text={"DODAJ LEKARZA"} primaryColor={"black"} secondaryColor={"white"} link={"/add/doctor"} />
+                    </li>
+                    <li>
+                        <HeaderButton text={"LISTA LEKARZY"} primaryColor={"black"} secondaryColor={"white"} link={"/show/doctors"} />
+                    </li>
+                    <li>
+                        <a href={"/"} className='buttons logout_button'>WYLOGUJ</a>
+                    </li>
+                </ul>
             </nav>
             )}
         default: {
